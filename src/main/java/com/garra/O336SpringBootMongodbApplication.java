e6a82e088880e159e0cb2322b1898b1ee4702293
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-07-13 11:19:42 -0300

    projeto criado

---
+++ src/main/java/com/garra/O336SpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.garra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O336SpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O336SpringBootMongodbApplication.class, args);
+	}
+
+}
